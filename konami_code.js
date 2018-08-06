2aff7ffe00a629dfa6f08985fba60f98f15d3308
Nicholas Powers <njpowers7915@gmail.com>
2018-08-06 15:05:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ let index = 0;
 
 function init(e) {
   // Write your JavaScript code inside the init() function
-  const key = parseInt(e.key);
+  const key = parseInt(e.detail || e.which);
   if (key === code[index]) {
     index++;
     if (index === code.length) {
